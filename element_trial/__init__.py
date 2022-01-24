8342d9e22eefceaa56d7f64ebedaecbaec63b09c
Chris Brozdowski <CBrozdowski@yahoo.com>
2022-01-24 12:07:02 -0600

    Apply suggestions from code review
    Co-authored-by: Kabilar Gunalan <kabilar@datajoint.com>

--- element_trial/__init__.py
+++ element_trial/__init__.py
@@ -1,12 +1,3 @@
-__author__ = "DataJoint"
-__date__ = "December, 2021"
-__version__ = "0.1.0c0"
-
-__all__ = ['__author__', '__version__', '__date__']
-
-import datajoint as dj
-dj.config['enable_python_native_blobs'] = True
-
 """
 Two schemas, trial and event. Trial for fully trialized, segmented.
 Event for events independent of trials, like an act of naturaistic behavior.
